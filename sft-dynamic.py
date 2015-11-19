47e3fec64eda32d8de0671b767903331fd4cd6b1
nycz <pyntix@gmail.com>
2015-11-19 01:45:11 +0100

    Added type annotations and some misc stuff
    Added traceback and removed some crap here and there

--- sft-dynamic.py
+++ sft-dynamic.py
@@ -5,27 +5,24 @@ from datetime import datetime, timedelta
 import os.path
 import pprint
 import struct
+import traceback
 import zlib
 
-from common import parse_refid, uint, uint8, uint16, uint32
+from typing import Any, Dict, List, Tuple
 
-def parse_uint(bits, i, data):
+from common import parse_refid, get_formid_data, uint, uint8, uint16, uint32
+
+def parse_uint(bits: int, i: int, data: bytes) -> Tuple[int, int]:
     return bits//8, uint(bits, data[i:i+4])
-    #if bits == 32:
-    #    return 4, uint32(data[i:i+4])
-    #elif bits == 16:
-    #    return 2, uint16(data[i:i+2])
-    #elif bits == 8:
-    #    return 1, uint8(data[i:i+1])
 
-def parse_float32(i, data):
+def parse_float32(i: int, data: bytes) -> float:
     return struct.unpack('f', data[i:i+4])[0]
 
-def parse_wstring(i, data):
+def parse_wstring(i: int, data: bytes) -> Tuple[int, str]:
     length = uint16(data[i:i+2])
     return 2 + length, data[i+2:i+2+length].decode()
 
-def parse_vsval(i, data):
+def parse_vsval(i: int, data: bytes) -> Tuple[int, int]:
     byte1 = data[i]
     # The two rightmost bits of the first byte decides the length of the var
     size = byte1 & 0b11
@@ -36,41 +33,48 @@ def parse_vsval(i, data):
         # uint16
         return 2, (byte1 | (data[i+1] << 8)) >> 2
     elif size == 2:
-        print('FUCK')
+        raise Exception('WELL SHIT the vsval got a uint32. just great')
 
-def parse_refids(refidcount, i, data):
-    #TODO
-    return refidcount*3, data[i:i+refidcount*3]
+def parse_refids(refidcount: int, formidarray: List[int], pluginlist: List[str],
+                 i: int, data: bytes) -> Tuple[int, List[Tuple[str, int]]]:
+    formiddata = [] # type: List[Tuple[str, int]]
+    for _ in range(refidcount):
+        plugin, fid = get_formid_data(data[i:i+3], formidarray, pluginlist)
+        formiddata.append((plugin, fid))
+        i += 3
+    return refidcount*3, formiddata
 
-def parse_factions(factioncount, i, data):
+def parse_factions(factioncount: int, formidarray: List[int], pluginlist: List[str],
+                    i: int, data: bytes) -> Tuple[int, bytes]:
     #TODO
     return factioncount*4, data[i:i+factioncount*4]
 
-def parse_filetime(i, data):
+def parse_filetime(i: int, data: bytes):
     # TODO: this crap
     return data[i:i+8]
 
-def parse_screenshot(w, h, i, data):
+def parse_screenshot(w: int, h: int, i: int, data: bytes) -> Tuple[int, bytes]:
     return w*h*3, data[i:i+w*h*3]
 
-def parse_plugininfo(i, data):
+def parse_plugininfo(i: int, data: bytes) -> List[str]:
     pluginnum = uint8(data[i:i+1])
     n = i+1
-    plugins = []
+    plugins = [] # type: List[str]
     for _ in range(pluginnum):
         offset, pluginname = parse_wstring(n, data)
         n += offset
         plugins.append(pluginname)
     return plugins
 
-def parse_formidarray(formidarraycount, i, data):
-    formids = []
+def parse_formidarray(formidarraycount: int, i: int, data: bytes) -> Tuple[int, List[int]]:
+    formids = [] # type: List[int]
     for _ in range(formidarraycount):
-        formids.append(hex(uint32(data[i:i+4])))
+        formids.append(uint32(data[i:i+4]))
         i += 4
     return i, formids
 
-def find_player_changeform(cfcount, formidarray, i, data):
+def find_player_changeform(cfcount: int, formidarray: List[int],
+                           i: int, data: bytes) -> Tuple[bytes, int, int]:
     """
     Go through all ChangeForms until the player is found and return it.
 
@@ -110,12 +114,12 @@ def find_player_changeform(cfcount, formidarray, i, data):
 
 
 
-def parse_file(fname):
+def parse_file(fname: str) -> Dict[str, Any]:
     with open('skyrim.sft', 'r') as f:
         instructions = f.read().splitlines()
     with open(fname, 'rb') as f:
         data = f.read()
-    vardict = OrderedDict()
+    vardict = OrderedDict() # type: Dict[str, Any]
     i = 0
     for line in instructions:
         if line.startswith('#') or not line.strip():
@@ -170,20 +174,21 @@ def parse_file(fname):
     #if error:
     #    print('\n !!!!! ERROR !!!!!')
 
-def generate_flags(num):
+def generate_flags(num: int) -> List[int]:
     return [n for n, x in enumerate(bin(num)[2:][::-1]) if x == '1']
 
-def parse_player(vardict):
+def parse_player(vardict: Dict[str, Any]):
     if vardict['$player'][1] != 0:
         data = zlib.decompress(vardict['$player'][0])
         assert len(data) == vardict['$player'][1]
     else:
         data = vardict['$player'][0]
     flags = generate_flags(vardict['$player'][2])
-    print(flags)
+    formidarray = vardict['$formidarray']
+    pluginlist = vardict['$plugininfo']
     with open('skyrim-face.sft', 'r') as f:
         instructions = f.read().splitlines()
-    pldict = OrderedDict()
+    pldict = OrderedDict() # type: Dict[str, Any]
     i = 0
     skipthisflag = False
     try:
@@ -219,21 +224,21 @@ def parse_player(vardict):
                     count = int(args[1])
                 else:
                     count = pldict[args[1]]
-                offset, pldict[args[0]] = parse_refids(count, i, data)
+                offset, pldict[args[0]] = parse_refids(count, formidarray, pluginlist, i, data)
                 i += offset
             elif cmd == 'refid':
-                offset, pldict[args[0]] = parse_refids(1, i, data)
+                offset, pldict[args[0]] = parse_refids(1, formidarray, pluginlist, i, data)
                 i += offset
             elif cmd == 'factions':
                 count = pldict[args[1]]//4
-                offset, pldict[args[0]] = parse_factions(count, i, data)
+                offset, pldict[args[0]] = parse_factions(count, formidarray, pluginlist, i, data)
                 i += offset
             else:
                 raise SyntaxError('Unknown command: {}'.format(cmd))
         if data[i:]:
             print('REST', data[i:])
     except Exception as e:
-        print('FUCK', e)
+        traceback.print_exc()
     finally:
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(pldict)
