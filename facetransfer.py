785f5e22dab7019c338f157194c3df7bc7c8e062
nycz <pyntix@gmail.com>
2015-11-29 14:51:45 +0100

    Updated ui.py, can now load source/target files
    Info and screenshot is shown but nothing else is implemented yet

---
+++ facetransfer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import extract
+
+def get_ui_data(fname):
+    """
+    Return data useful for the UI to display, such as name, file info etc.
+    """
+    with open(fname, 'rb') as f:
+        rawdata = f.read()
+    game, data = extract.parse_savedata(rawdata)
+    out = {}
+    out['save number'] = data['savenumber']
+    out['name'] = data['playername']
+    out['level'] = data['playerlevel']
+    out['location'] = data['playerlocation']
+    out['race'] = data['playerraceeditorid']
+    out['gender'] = data['playersex']
+    out['playing time'] = data['gamedate']
+    out['screenshot'] = (data['shotwidth'], data['shotheight'], data['screenshotdata'])
+    return out
+
+
+def transfer_face(sourcefname: str, targetfname: str):
+    """
+    Copy facial data from one save file to another. This function should be
+    the main entry point for the UI.
+    """
+    with open(sourcefname, 'rb') as f:
+        sourcerawdata = f.read()
+    with open(targetfname, 'rb') as f:
+        targetrawdata = f.read()
+    sourcegame, sourcedata = extract.parse_savedata(sourcerawdata)
+    targetgame, targetdata = extract.parse_savedata(targetrawdata)
+    if sourcegame != targetgame:
+        raise Exception('Saves are not from the same game!')
+    if sourcedata['playersex'] != targetdata['playersex']:
+        raise Exception('Characters must have the same gender!')
+    if sourcedata['playerraceeditorid'] != targetdata['playerraceeditorid']:
+        raise Exception('Characters must be the same race!')
+    # Get the player data from the source save
+    sourcecfdata = extract.parse_changeforms(sourcedata['changeforms'])
+    sourceplayer = extract.parse_player(sourcecfdata['playerdata'],
+                                        sourcecfdata['playerchangeflags'],
+                                        sourcegame)
+    # Get the data from target save
+    targetcfdata = extract.parse_changeforms(targetdata['changeforms'])
+    targetplayer = extract.parse_player(targetcfdata['playerdata'],
+                                        targetcfdata['playerchangeflags'],
+                                        targetgame)
+    # Merge players, return target player with source's face
+    newplayer, newflags = extract.merge_player(
+        sourceplayer, sourcecfdata['playerchangeflags'],
+        targetplayer, targetcfdata['playerchangeflags'],
+        sourcegame
+    )
+    # Encode and put the new face and flags in the target changeform data
+    targetcfdata['playerdata'] = extract.encode_player(newplayer, sourcegame)
+    targetcfdata['playerchangeflags'] = newflags
+    # Encode the changeform data and put it in the target main data
+    targetdata['changeforms'] = extract.encode_changeforms(targetcfdata)
+    # Then encode the whole file
+    newrawdata = extract.encode_savedata(targetdata)
+    # Write do disk
+    # TODO: temporary
+    with open('out.fos', 'wb') as f:
+        f.write(newrawdata)
+
--- ui.py
+++ ui.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python3
 
+import os.path
+import tempfile
 from tkinter import *
 from tkinter.messagebox import *
+from tkinter import filedialog
 from tkinter.ttk import *
 
+import facetransfer
+
 version = '0.1'
+
+
+def formatPlayingTime(timestamp):
+    out = []
+    # Fallout 4 timestamp (does not include seconds)
+    if 'days' in timestamp:
+        t = [x[:-1].lstrip('0') for x in timestamp.split('.', 3)[:3]] + ['']
+    # Skyrim timestamp (does not include days)
+    else:
+        t = [''] + [x.lstrip('0') for x in timestamp.split('.')]
+    if t[0]:
+        out.append(t[0] + 'days')
+    if t[1]:
+        out.append(t[1] + 'h')
+    if t[2]:
+        out.append(t[2] + 'min')
+    if t[3]:
+        out.append(t[3] + 's')
+    return ' '.join(out)
 
 
 class MainWindow(Frame):
     def __init__(self, root):
         super().__init__(root)
-        root.title('Fallout 4 Face Transfer')
+        root.title('Face Transfer')
+        # Global~ish variables
+        #self.savedir = getSavePath()
+        self.wildcard = [('Fallout 4 save files', '*.fos'),
+                         ('Skyrim save files', '*.ess'),
+                         ('All files', '*')]
         # Menu
         menubar = Menu(self)
         menu = Menu(menubar, tearoff=0)
-        menu.add_command(label='About', command=self.showAboutDialog)
-        menu.add_command(label='Help', command=self.showHelpDialog)
+        menu.add_command(label='About', command=self.show_about_dialog)
         menu.add_command(label='Visit Nexus page')
         menu.add_command(label='Quit', command=lambda:root.destroy())
         menubar.add_cascade(label='Menu', menu=menu)
@@ -25,23 +53,23 @@ class MainWindow(Frame):
         self.screen = {}
         self.widgets = {}
         self.screenshot = {}
-        doBrowse = {'source': self.sourceBrowse, 'target': self.targetBrowse}
+        browsefuncs = {'source': self.source_browse, 'target': self.target_browse}
         for t in ('source', 'target'):
             self.field[t] = StringVar()
             self.widgets[t] = {}
-            self.addFileInfoBox(self, t, self.widgets[t], doBrowse[t],
+            self.add_file_info_box(self, t, self.widgets[t], browsefuncs[t],
                                 self.field[t])
             Frame(self, height=10).pack(side=TOP)
 
         # Bottom part
-        self.btn_transfer = Button(self, text='Transfer', state=DISABLED)
-        self.btn_transfer.pack(side=RIGHT)
+        self.btntransfer = Button(self, text='Transfer', state=DISABLED)
+        self.btntransfer.pack(side=RIGHT)
         # Warning labels
         self.warninglabelvar = StringVar()
         self.warninglabel = Label(root, textvariable=self.warninglabelvar)
         self.pack(fill=BOTH, expand=1, padx=8, pady=8)
 
-    def addFileInfoBox(self, mainframe, name, labeldict, btncallback, fvar):
+    def add_file_info_box(self, mainframe, name, labeldict, btncallback, fvar):
         """
         Create and add a infobox containing the info about a loaded
         savegame.
@@ -70,41 +98,58 @@ class MainWindow(Frame):
         self.screenshot[name].grid(column=2, row=1, rowspan=len(l),
                                    padx=4, pady=4)
 
-
-    def showAboutDialog(self):
+    def show_about_dialog(self):
         import platform
-        showinfo('About Skyrim Face Transfer', 'Skyrim Face Transfer {0} \n\n'
-                 'Programmed in 2012 by nycz.\n\nLicensed under GPL. See the '
-                 'sourcecode for more info.\n\nMade to run on Python 2.6 or '
-                 'above (not Python 3).\n\nRunning on python version: {1}'
+        showinfo('About Face Transfer', 'Face Transfer {0} \n\n'
+                 'Programmed in 2015 by nycz.\n\nLicensed under GPL. See the '
+                 'sourcecode for more info.\n\nMade to run on Python 3.4 or '
+                 'above.\n\nRunning on python version: {1}'
                  ''.format(version, platform.python_version()))
 
-    def showHelpDialog(self):
-        showinfo('Help (You should probably read the readme instead)',
-                 "Skyrim Face Transfer lets you transfer your "
-                 "character's face from one savefile to another. Just choose "
-                 "the file with the face you want to reuse (the source), and "
-                 "then the file with the character you want the face to go to. "
-                 "\n\nIt is best if both characters are of the same race, "
-                 "since the function to copy the race is currently bugged. "
-                 "Even so, you are free to try it out anyway. Also, "
-                 "transferring a face between characters with different race "
-                 "and/or gender WITHOUT transferring the gender/race, may look "
-                 "odd. (The target will have the facial traits of the source "
-                 "gender/race, but still be the target's gender/race in every "
-                 "other way).\n\nFinally, SFT creates a backup of the target "
-                 "save before modifying it but if you want to be on the safe "
-                 "side you are still recommended to backup your saves before "
-                 "using this program.")
+    def source_browse(self):
+        fname = filedialog.askopenfilename(filetypes=self.wildcard)
+        self.browse(fname, 'source')
 
-    def sourceBrowse(self):
-        self.browse('source')
+    def target_browse(self):
+        fname = filedialog.askopenfilename(filetypes=self.wildcard)
+        self.browse(fname, 'target')
 
-    def targetBrowse(self):
-        self.browse('target')
+    def browse(self, fname, t):
+        if not fname:
+            return
+        fname = os.path.normpath(fname)
+        self.field[t].set(fname)
+        info = facetransfer.get_ui_data(fname)
+        for k,v in info.items():
+            if k == 'screenshot':
+                w, h, shotdata = v
+                if len(shotdata)/w/h == 4:
+                    shotdata = bytes([b for n,b in enumerate(shotdata) if (n+1)%4 != 0])
+                tfile = tempfile.NamedTemporaryFile('wb', suffix='.ppm',
+                                                    delete=False)
+                if h == 384:
+                    rows = list(zip(*[iter(shotdata)]*w*3))
+                    tempshotdata = []
+                    for y in range(0, len(rows), 2):
+                        for x in range(0, len(rows[0]), 6):
+                            for n in [0,1,2]:
+                                newval = int((rows[y][x+n] + rows[y][x+n+3] + rows[y+1][x+n] + rows[y+1][x+n+3])/4)
+                                tempshotdata.append(newval)
+                    shotdata = bytes(tempshotdata)
+                    w, h = w//2, h//2
+                with tfile:
+                    tfile.write('P6\n{0} {1}\n255\n'.format(w,h).encode())
+                    tfile.write(shotdata)
+                img = PhotoImage(file=tfile.name)
+                os.remove(tfile.name) # Remove the tempfile
+                self.screenshot[t].config(image=img, relief=SUNKEN)
+                self.screenshot[t].image = img
+            elif k == 'playing time':
+                self.widgets[t][k].set(formatPlayingTime(v))
+            else:
+                self.widgets[t][k].set(v)
 
-    def browse(self, t):
-        print('beep boop', t)
+
 
 if __name__=='__main__':
     root = Tk()
